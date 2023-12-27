d0a5142819797f9703541881a1cb5f8847a31796
Heitor Eduardo Avelar Theodoro Canhizares <44684991+HeitorCanhizares@users.noreply.github.com>
2023-12-27 09:37:31 -0400

    Add files via upload

---
+++ target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\dev10\hello_world\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\dev10\hello_world\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\dev10\hello_world\hello_cargo\target\debug\hello_cargo.exe: C:\Users\dev10\hello_world\hello_cargo\src\main.rs
